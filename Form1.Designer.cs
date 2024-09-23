2ea5dfea81bbe4ccab6088fe03c399f72e9e0887
NoviceBosSystems <ryangregoire286@gmail.com>
2024-09-23 13:42:38 -0400

    Added And Changed Name of Application

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,38 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            textBox2 = new TextBox();
             textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            listBox1 = new ListBox();
             SuspendLayout();
-            // 
-            // button1
-            // 
-            button1.Location = new Point(124, 244);
-            button1.Name = "button1";
-            button1.Size = new Size(125, 23);
-            button1.TabIndex = 0;
-            button1.Text = "Add Cheat";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
-            // 
-            // textBox2
-            // 
-            textBox2.Location = new Point(64, 130);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(272, 23);
-            textBox2.TabIndex = 3;
-            textBox2.Text = "Process Name";
-            textBox2.TextChanged += textBox2_TextChanged;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(64, 180);
+            textBox1.Location = new Point(54, 170);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(272, 23);
-            textBox1.TabIndex = 4;
+            textBox1.TabIndex = 7;
             textBox1.Text = "Address +  Offset";
-            textBox1.TextChanged += textBox1_TextChanged_1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(54, 84);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(272, 23);
+            textBox2.TabIndex = 6;
+            textBox2.Text = "Process Name";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(116, 235);
+            button1.Name = "button1";
+            button1.Size = new Size(125, 23);
+            button1.TabIndex = 5;
+            button1.Text = "Add Cheat";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(12, 12);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(357, 319);
+            listBox1.TabIndex = 0;
             // 
             // Form1
             // 
@@ -69,17 +76,19 @@
             Controls.Add(textBox1);
             Controls.Add(textBox2);
             Controls.Add(button1);
+            Controls.Add(listBox1);
             Name = "Form1";
-            Text = "Form1";
+            Text = "GameOffsetTool";
             Load += Form1_Load;
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
-        private Button button1;
-        private TextBox textBox2;
         private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private Label label1;
+        private ListBox listBox1;
     }
 }
